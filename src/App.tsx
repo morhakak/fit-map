2024af467a85d846076f15f9563ad3a7e8eda3f6
mor_hakak <morhak6819@gmail.com>
2025-07-13 00:26:07 +0300

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import FacilitiesList from "./components/FacilitiesList";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 function App() {
-  return <FacilitiesList />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <FacilitiesList />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/components/FacilitiesList.tsx
+++ src/components/FacilitiesList.tsx
@@ -8,6 +8,7 @@ import { fromITMtoWGS84 } from "@/utils/projection";
 import FacilitySearchUI from "@/components/FacilitySearchUI";
 import FacilityListPanel from "./FacilityListPanel";
 import FacilityMap from "./FacilityMap";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 const FacilitiesList = () => {
   const [facilities, setFacilities] = useState<Facility[]>([]);
@@ -17,12 +18,102 @@ const FacilitiesList = () => {
   );
   const [cityQuery, setCityQuery] = useState("");
   const [previousCityQuery, setPreviousCityQuery] = useState<string>(cityQuery);
-  const [isLoading, setIsLoading] = useState(false);
   const [typeFilter, setTypeFilter] = useState<string[]>([]);
   const [isListOpen, setIsListOpen] = useState(false);
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   const [errorMessage, setErrorMessage] = useState("");
   const { userLocation, resolvedCity } = useUserLocation();
+
+  const qc = useQueryClient();
+  const queryKey = ["facilities", cityQuery];
+  const { data, isLoading, error, refetch } = useQuery<Facility[], Error>({
+    queryKey: queryKey,
+    queryFn: async () => {
+      const response = await axios.get("/api/facilities", {
+        params: { q: cityQuery },
+      });
+
+      const raws = response.data.result.records as RawFacility[];
+      const filteredRaw = raws.filter((f) => f["רשות מקומית"] === cityQuery);
+      return filteredRaw.map((f) => {
+        const x = Number(f["ציר X"]);
+        const y = Number(f["ציר Y"]);
+        const { lat, lng } = fromITMtoWGS84(x, y);
+        return {
+          id: f._id,
+          name: f["שם המתקן"],
+          lat,
+          lng,
+          street: f["רחוב"],
+          houseNumber: f["מספר בית"],
+          type: f["סוג מתקן"],
+          schoolServed: Boolean(f["משרת בית ספר"]),
+          availability: f["פנוי לפעילות"],
+          accessibility: Boolean(f["נגישות לנכים"]),
+          status: f["מצב המתקן"],
+        };
+      });
+    },
+    enabled: false,
+    staleTime: Infinity,
+  });
+
+  useEffect(() => {
+    console.error(error);
+    if (error) {
+      const msg =
+        "אופס! נראה שיש לנו בעיה בקבלת הנתונים כרגע אנא נסה/נסי שוב מאוחר יותר.";
+      toast.error(msg);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (data) {
+      setAllFacilities(data);
+      if (data.length > 0) {
+        setIsListOpen(true);
+      } else {
+        toast.info("לא נמצאו מתקנים ברשות המקומית שהזנת");
+      }
+      setErrorMessage("");
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (allFacilities.length > 0) {
+      const filtered =
+        typeFilter.length === 0
+          ? allFacilities
+          : allFacilities.filter((f) =>
+              typeFilter.some((t) => f.type?.includes(t))
+            );
+      setFacilities(filtered);
+    }
+  }, [typeFilter, allFacilities]);
+
+  const handleSearch = async () => {
+    if (!isValidHebrewInput(cityQuery) || !cityQuery.trim()) {
+      toast.warning("יש להזין עד 50 תווים בעברית בלבד");
+      return;
+    }
+
+    const cached = qc.getQueryData<Facility[]>(queryKey);
+
+    if (cached) {
+      setAllFacilities(cached);
+      setFacilities(cached);
+      setIsListOpen(cached.length > 0);
+      return;
+    }
+
+    if (cityQuery !== previousCityQuery) {
+      setPreviousCityQuery(cityQuery);
+      setSelectedFacility(null);
+      setTypeFilter([]);
+    }
+    setErrorMessage("");
+    refetch();
+  };
 
   useEffect(() => {
     if (resolvedCity) {
@@ -42,80 +133,9 @@ const FacilitiesList = () => {
     }
   }, [typeFilter, allFacilities]);
 
-  const isValidInput = (text: string): boolean => {
+  const isValidHebrewInput = (text: string): boolean => {
     const hebrewRegex = /^[\u0590-\u05FF\s]+$/;
     return hebrewRegex.test(text.trim()) && text.trim().length <= 50;
-  };
-
-  const handleSearch = async () => {
-    if (!cityQuery.trim()) return;
-
-    if (!isValidInput(cityQuery)) {
-      toast.warning("יש להזין עד 50 תווים בעברית בלבד");
-      return;
-    }
-    if (cityQuery !== previousCityQuery) {
-      setFacilities([]);
-      setAllFacilities([]);
-      setSelectedFacility(null);
-      setPreviousCityQuery(cityQuery);
-    }
-
-    setIsLoading(true);
-    try {
-      const response = await axios.get("/api/facilities", {
-        params: { q: cityQuery },
-      });
-
-      const raws = response.data.result.records as RawFacility[];
-
-      const justThisMunicipality = raws.filter(
-        (f) => f["רשות מקומית"] === cityQuery
-      );
-
-      const mapped: Facility[] = justThisMunicipality.map((f) => {
-        const x = Number(f["ציר X"]);
-        const y = Number(f["ציר Y"]);
-        const { lat, lng } = fromITMtoWGS84(x, y);
-
-        return {
-          id: f._id,
-          name: f["שם המתקן"],
-          lat,
-          lng,
-          street: f["רחוב"],
-          houseNumber: f["מספר בית"],
-          type: f["סוג מתקן"],
-          schoolServed: !!f["משרת בית ספר"],
-          availability: f["פנוי לפעילות"],
-          accessibility: Boolean(f["נגישות לנכים"]),
-          status: f["מצב המתקן"],
-        };
-      });
-
-      setAllFacilities(mapped);
-
-      const filtered =
-        typeFilter.length === 0
-          ? mapped
-          : mapped.filter((f) =>
-              typeFilter.some((type) => f.type?.includes(type))
-            );
-      setFacilities(filtered);
-
-      if (filtered.length > 0) {
-        setIsListOpen(true);
-      }
-
-      if (mapped.length === 0) {
-        toast.info("לא קיימים מתקני ספורט ברשות המקומית שהזנת");
-      }
-    } catch (error) {
-      console.error("שגיאה בשליפת מתקנים:", error);
-      toast.error("אירעה שגיאה במהלך החיפוש. נסה שוב מאוחר יותר.");
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   const toggleType = (type: string) => {
